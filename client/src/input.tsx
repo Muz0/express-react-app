dd5221f57a84239f96144ba217397021fc20176e
Muz0 <muzaffar.mustafaev1@gmail.com>
2023-09-25 21:06:44 +0400

    initial commit

---
+++ client/src/input.tsx
@@ -0,0 +1,13 @@
+// create a simple input component
+
+import React from 'react';
+
+export default function Input() {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+}
+
+// Path: client/src/App.tsx
